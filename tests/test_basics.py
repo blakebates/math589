a043451f3da264dd204fa3b49877fec0484e7362
Marek Rychlik <rychlik@arizona.edu>
2024-04-24 11:15:44 -0700

    *** empty log message ***

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -2,7 +2,7 @@ import sys
 import pytest
 sys.path.append("..")
 
-from "control_pendulum" import control_pendulum
+from control_pendulum import control_pendulum
 
 def test_basic():
   state,cost = control_pendulum(0, .03, 1/10)
